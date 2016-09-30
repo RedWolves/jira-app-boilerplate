e35861b61277a9765069ead8188222339788d87b
Ralph Whitbeck <rwhitbeck@atlassian.com>
2016-09-30 19:36:54 -0400

    Updated code so atlassian-connect.json is generated with the ngrok url that's created.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/atlassian-connect.json', (req, res) => {
+  var descriptor = JSON.parse(fs.readFileSync('generated/atlassian-connect.json'));
+  res.send(200, descriptor);
+  res.end();
 });
 
 module.exports = router;
