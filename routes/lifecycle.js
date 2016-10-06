6e8f148e4c141413ad828c0ddd0d3e23f35fcf65
Ralph Whitbeck <rwhitbeck@atlassian.com>
2016-10-05 23:57:12 -0400

    Cleaned up debug statements

--- routes/lifecycle.js
+++ routes/lifecycle.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var debug = require('debug')('JIRA:server');
+var debug = require('debug')('JIRA:lifecycle');
 
 router.post("/installed", (req, res, err) => {
-  debug("Lifecycle: installed");
+  debug("Webhook posted: installed");
   debug("Installation payload:");
   console.log(req.body);
   res.sendStatus(200);
@@ -11,21 +11,21 @@ router.post("/installed", (req, res, err) => {
 });
 
 router.post("/uninstalled", (req, res, err) => {
-  debug("Lifecycle: uninstalled");
+  debug("Webhook posted: uninstalled");
   console.log(req.body);
   res.sendStatus(200);
   res.end();
 });
 
 router.post("/enabled", (req, res, err) => {
-  debug("Lifecycle: enabled");
+  debug("Webhook posted: enabled");
   console.log(req.body);
   res.sendStatus(200);
   res.end();
 });
 
 router.post("/disabled", (req, res, err) => {
-  debug("Lifecycle: disabled");
+  debug("Webhook posted: disabled");
   console.log(req.body);
   res.sendStatus(200);
   res.end();
